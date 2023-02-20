f1efc2c7acd65ec5ce9dd7f1e7bdb92539082f92
nmartins0611 <62961138+nmartins0611@users.noreply.github.com>
2023-02-20 12:54:13 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -7,6 +7,7 @@ sed -i 's~/rhel/~/centos/~g' /etc/yum.repos.d/docker-ce.repo
 yum --noplugins install docker-ce docker-ce-cli containerd.io docker-compose-plugin --allowerasing -y
 systemctl enable docker
 systemctl start docker
+setenforce 0
 
 # Grab sample switch config
 mkdir /tmp/setup
@@ -16,32 +17,31 @@ chmod -R 777 /tmp/setup
 
 ### Configure containers
 
+## Create Networks
+
+docker network create net1
+docker network create net2
 
 ## Create Switch configs with Starting config
 
-#docker create --name=ceos1 --privileged -v /tmp/Instruqt_netops/sw01/sw01.conf:/mnt/flash/startup-config -e INTFTYPE=eth -e ETBA=1 -e SKIP_ZEROTOUCH_BARRIER_IN_SYSDBINIT=1 -e CEOS=1 -e EOS_PLATFORM=ceoslab -e container=docker -i -t quay.io/nmartins/ceoslab-rh /sbin/init systemd.setenv=INTFTYPE=eth systemd.setenv=ETBA=1 systemd.setenv=SKIP_ZEROTOUCH_BARRIER_IN_SYSDBINIT=1 systemd.setenv=CEOS=1 systemd.setenv=EOS_PLATFORM=ceoslab systemd.setenv=container=docker
-#docker create --name=ceos2 --privileged -v /tmp/Instruqt_netops/sw02/sw02.conf:/mnt/flash/startup-config -e INTFTYPE=eth -e ETBA=1 -e SKIP_ZEROTOUCH_BARRIER_IN_SYSDBINIT=1 -e CEOS=1 -e EOS_PLATFORM=ceoslab -e container=docker -i -t quay.io/nmartins/ceoslab-rh /sbin/init systemd.setenv=INTFTYPE=eth systemd.setenv=ETBA=1 systemd.setenv=SKIP_ZEROTOUCH_BARRIER_IN_SYSDBINIT=1 systemd.setenv=CEOS=1 systemd.setenv=EOS_PLATFORM=ceoslab systemd.setenv=container=docker
-#docker create --name=ceos3 --privileged -v /tmp/Instruqt_netops/sw03/sw03.conf:/mnt/flash/startup-config -e INTFTYPE=eth -e ETBA=1 -e SKIP_ZEROTOUCH_BARRIER_IN_SYSDBINIT=1 -e CEOS=1 -e EOS_PLATFORM=ceoslab -e container=docker -i -t quay.io/nmartins/ceoslab-rh /sbin/init systemd.setenv=INTFTYPE=eth systemd.setenv=ETBA=1 systemd.setenv=SKIP_ZEROTOUCH_BARRIER_IN_SYSDBINIT=1 systemd.setenv=CEOS=1 systemd.setenv=EOS_PLATFORM=ceoslab systemd.setenv=container=docker
-
-## Create Networks
-
-#docker network create net1
-#docker network create net2
+docker create --name=ceos1 --privileged -v /tmp/setup/sw01/sw01.conf:/mnt/flash/startup-config -e INTFTYPE=eth -e ETBA=1 -e SKIP_ZEROTOUCH_BARRIER_IN_SYSDBINIT=1 -e CEOS=1 -e EOS_PLATFORM=ceoslab -e container=docker -i -t quay.io/nmartins/ceoslab-rh /sbin/init systemd.setenv=INTFTYPE=eth systemd.setenv=ETBA=1 systemd.setenv=SKIP_ZEROTOUCH_BARRIER_IN_SYSDBINIT=1 systemd.setenv=CEOS=1 systemd.setenv=EOS_PLATFORM=ceoslab systemd.setenv=container=docker
+docker create --name=ceos2 --privileged -v /tmp/setup/sw02/sw02.conf:/mnt/flash/startup-config -e INTFTYPE=eth -e ETBA=1 -e SKIP_ZEROTOUCH_BARRIER_IN_SYSDBINIT=1 -e CEOS=1 -e EOS_PLATFORM=ceoslab -e container=docker -i -t quay.io/nmartins/ceoslab-rh /sbin/init systemd.setenv=INTFTYPE=eth systemd.setenv=ETBA=1 systemd.setenv=SKIP_ZEROTOUCH_BARRIER_IN_SYSDBINIT=1 systemd.setenv=CEOS=1 systemd.setenv=EOS_PLATFORM=ceoslab systemd.setenv=container=docker
+docker create --name=ceos3 --privileged -v /tmp/setup/sw03/sw03.conf:/mnt/flash/startup-config -e INTFTYPE=eth -e ETBA=1 -e SKIP_ZEROTOUCH_BARRIER_IN_SYSDBINIT=1 -e CEOS=1 -e EOS_PLATFORM=ceoslab -e container=docker -i -t quay.io/nmartins/ceoslab-rh /sbin/init systemd.setenv=INTFTYPE=eth systemd.setenv=ETBA=1 systemd.setenv=SKIP_ZEROTOUCH_BARRIER_IN_SYSDBINIT=1 systemd.setenv=CEOS=1 systemd.setenv=EOS_PLATFORM=ceoslab systemd.setenv=container=docker
 
 ## Attach Networks
 
-#docker network connect net1 ceos1
-#docker network connect net2 ceos1
-#docker network connect net1 ceos2
-#docker network connect net2 ceos2
-#docker network connect net1 ceos3
-#docker network connect net2 ceos3
+docker network connect net1 ceos1
+docker network connect net2 ceos1
+docker network connect net1 ceos2
+docker network connect net2 ceos2
+docker network connect net1 ceos3
+docker network connect net2 ceos3
 
 ## Start Switches
 
-#docker start ceos1
-#docker start ceos2
-#docker start ceos3
+docker start ceos1
+docker start ceos2
+docker start ceos3
 
 ## Each tab loads a switch
 
