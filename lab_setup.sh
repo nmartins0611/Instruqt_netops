299491546bbdb8fdbdcf53aace4de14be8bb6896
nmartins0611 <62961138+nmartins0611@users.noreply.github.com>
2023-04-20 19:48:24 +0200

    Update lab_setup.sh

--- lab_setup.sh
+++ lab_setup.sh
@@ -1,13 +1,10 @@
-#/bin/bash
-# Terraform Lab Setup Script
-#sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/RHEL/hashicorp.repo
-sudo yum -y localinstall  /tmp/setup-scripts/terraform-ansible/terraform-1.2.8-1.x86_64.rpm
+
+
+sudo yum install ansible-core
+ansible-galaxy collection install community.general
 
 # Configure gitea and repo for builds
-ansible-playbook /tmp/setup-scripts/terraform-ansible/gitea_setup.yml -e @/tmp/setup-scripts/terraform-ansible/track_vars.yml -i /tmp/setup-scripts/terraform-ansible/inventory.ini
+ansible-playbook gitea_setup.yml -e @track_vars.yml -i inventory.ini
 
-# Configure Repo for builds
-ansible-playbook /tmp/setup-scripts/terraform-ansible/gitea_build.yml -e @/tmp/setup-scripts/terraform-ansible/track_vars.yml -i /tmp/setup-scripts/terraform-ansible/inventory.ini
 
-# Configure controller environment 
-ansible-playbook /tmp/setup-scripts/terraform-ansible/controller_setup.yml -e @/tmp/setup-scripts/terraform-ansible/track_vars.yml -i /tmp/setup-scripts/terraform-ansible/inventory.ini
+
